930e42d0a966fec974fe472ac33b428666efb60c
kiarash14 <omidbakhsh22@gmail.com>
2016-02-24 15:51:25 +0330

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -98,7 +98,7 @@ end
 -- !help command
 local function telegram_help()
   local i = 0
-  local text = "Plugins list:\n\n"
+  local text = "Plugins Bumper list:\n\n"
   -- Plugins names
   for name in pairsByKeys(plugins) do
     if plugins[name].hidden then
@@ -108,9 +108,9 @@ local function telegram_help()
     text = text..i..'. '..name..'\n'
     end
   end
-  text = text..'\n'..'There are '..i..' plugins help available.'
-  text = text..'\n'..'Write "!help [plugin name]" or "!help [plugin number]" for more info.'
-  text = text..'\n'..'Or "!help all" to show all info.'
+  text = text..'\n'..'There are '..i..' plugins Bumper help available.'
+  text = text..'\n'..'Write "help [plugin name]" or "help [plugin number]" for more info.'
+  text = text..'\n'..'Or "help all" to show all info.'
   return text
 end
  
@@ -138,9 +138,9 @@ local function run(msg, matches)
   else
       requester = "user"
   end
-  if matches[1] == "!help" then
+  if matches[1] == "help" then
     return telegram_help()
-  elseif matches[1] == "!help all" then
+  elseif matches[1] == "help all" then
     return help_all(requester)
   else
     local text = ""
@@ -159,15 +159,15 @@ end
 return {
   description = "Help plugin. Get info from other plugins.  ",
   usage = {
-    "!help: Show list of plugins.",
-    "!help all: Show all commands for every plugin.",
-    "!help [plugin name]: Commands for that plugin.",
-    "!help [number]: Commands for that plugin. Type !help to get the plugin number."
+    "help: Show list of plugins.",
+    "help all: Show all commands for every plugin.",
+    "help [plugin name]: Commands for that plugin.",
+    "help [number]: Commands for that plugin. Type !help to get the plugin number."
   },
   patterns = {
-    "^!help$",
-    "^!help all",
-    "^!help (.+)"
+    "^help$",
+    "^help all",
+    "^help (.+)"
   },
   run = run
 }
