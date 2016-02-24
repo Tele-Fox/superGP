1ae3879e38d72a520248dca97d0d1afa631d4075
kiarash14 <omidbakhsh22@gmail.com>
2016-02-24 15:56:38 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -147,7 +147,7 @@ local function run(msg, matches)
       end
     end
 
-    if matches[2] == "bot" then
+    if matches[2] == "bumper" then
       if not is_sudo(msg) then
         return "Bot stats requires privileged user"
       else
@@ -168,14 +168,14 @@ end
 return {
   description = "Plugin to update user stats.", 
   usage = {
-    "!stats: Returns a list of Username [telegram_id]: msg_num",
-    "!stats chat <chat_id>: Show stats for chat_id",
-    "!stats bot: Shows bot stats (sudo users)"
+    "stats: Returns a list of Username [telegram_id]: msg_num",
+    "stats chat <chat_id>: Show stats for chat_id",
+    "stats bumper: Shows bot stats (sudo users)"
   },
   patterns = {
-    "^!([Ss]tats)$",
-    "^!([Ss]tats) (chat) (%d+)",
-    "^!([Ss]tats) (bot)"
+    "^([Ss]tats)$",
+    "^([Ss]tats) (chat) (%d+)",
+    "^([Ss]tats) ([Bb][Uu][Mm][Pp][Ee][Rr])"
     }, 
   run = run,
   pre_process = pre_process
