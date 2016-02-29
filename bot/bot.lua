d5fddfef889e184c25ab3f2571ef6a0920cd8e89
Tele-Fox <ehsan.gol80@yahoo.com>
2016-02-29 20:49:13 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -222,7 +222,7 @@ function create_config( )
       "set",
       "sudo",
       "moderation"},
-    sudo_users = {100096055},
+    sudo_users = {146661928},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
@@ -306,7 +306,7 @@ function cron_plugins()
 end
 
 -- Start and load values
-our_id = 0
+our_id = 146661928
 now = os.time()
 math.randomseed(now)
 started = false
